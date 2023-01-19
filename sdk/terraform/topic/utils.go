6ea4d8c767152deadf612e5cb8bc56fab79e7d0b
Maksim Zherebyatew <maxijer@yandex-team.ru>
2023-01-19 18:40:42 +0300

    fix resource, data_source and utils

--- sdk/terraform/topic/utils.go
+++ sdk/terraform/topic/utils.go
@@ -1,22 +1,16 @@
 package topic
 
-//func getIAMToken(ctx context.Context, d ResourceDataProxy, config *Config) (string, error) {
-//	if d != nil {
-//		res, ok := d.GetOk("iam_token")
-//		if ok {
-//			return res.(string), nil
-//		}
-//	}
-//	if config.ServiceAccountKeyFile != "" {
-//		response, err := config.sdk.CreateIAMToken(ctx)
-//		if err != nil {
-//			return "", err
-//		}
-//		return response.IamToken, nil
-//	}
-//	token, err := config.sdk.CreateIAMToken(ctx)
-//	if err != nil {
-//		return "", fmt.Errorf("cannot determine IAM token: please set 'iam_token' attr in this resource or use service account key file at provider level")
-//	}
-//	return token.IamToken, nil
-//}
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"time"
+)
+
+func defaultTimeouts() *schema.ResourceTimeout {
+	return &schema.ResourceTimeout{
+		Create:  schema.DefaultTimeout(time.Minute * 20),
+		Read:    schema.DefaultTimeout(time.Minute * 20),
+		Update:  schema.DefaultTimeout(time.Minute * 20),
+		Delete:  schema.DefaultTimeout(time.Minute * 20),
+		Default: schema.DefaultTimeout(time.Minute * 20),
+	}
+}
