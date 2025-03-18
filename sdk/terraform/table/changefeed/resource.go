05d48a64673581caed29552ff470058ecfbcf52d
Alexander Kitaev <alkitaev@yandex-team.ru>
2025-03-18 15:28:58 +0300

    Add description field to resources

--- sdk/terraform/table/changefeed/resource.go
+++ sdk/terraform/table/changefeed/resource.go
@@ -107,6 +107,7 @@ func ResourceSchema() map[string]*schema.Schema {
 	return map[string]*schema.Schema{
 		"table_path": {
 			Type:         schema.TypeString,
+			Description:  "Table path.",
 			Optional:     true,
 			Computed:     true,
 			ValidateFunc: validation.All(validation.NoZeroValues, helpers.YdbTablePathCheck),
@@ -117,6 +118,7 @@ func ResourceSchema() map[string]*schema.Schema {
 		},
 		"connection_string": {
 			Type:         schema.TypeString,
+			Description:  "Connection string, conflicts with `table_id`.",
 			Optional:     true,
 			Computed:     true,
 			ValidateFunc: validation.NoZeroValues,
@@ -126,10 +128,11 @@ func ResourceSchema() map[string]*schema.Schema {
 			},
 		},
 		"table_id": {
-			Type:     schema.TypeString,
-			Optional: true,
-			ForceNew: true,
-			Computed: true,
+			Type:        schema.TypeString,
+			Description: "Terraform resource ID of the table.",
+			Optional:    true,
+			ForceNew:    true,
+			Computed:    true,
 			ConflictsWith: []string{
 				"table_path",
 				"connection_string",
@@ -137,46 +140,54 @@ func ResourceSchema() map[string]*schema.Schema {
 		},
 		"name": {
 			Type:         schema.TypeString,
+			Description:  "Changefeed name.",
 			Required:     true,
 			ValidateFunc: validation.NoZeroValues,
 			ForceNew:     true,
 		},
 		"mode": {
 			Type:         schema.TypeString,
+			Description:  "[Changefeed mode](https://ydb.tech/en/docs/yql/reference/syntax/alter_table#changefeed-options).",
 			Required:     true,
 			ValidateFunc: validation.NoZeroValues,
 			ForceNew:     true,
 		},
 		"format": {
-			Type:     schema.TypeString,
-			Required: true,
-			ForceNew: true,
+			Type:        schema.TypeString,
+			Description: "Changefeed format.",
+			Required:    true,
+			ForceNew:    true,
 		},
 		"virtual_timestamps": {
-			Type:     schema.TypeBool,
-			Optional: true,
-			ForceNew: true,
+			Type:        schema.TypeBool,
+			Description: "Use [virtual timestamps](https://ydb.tech/en/docs/concepts/cdc#virtual-timestamps).",
+			Optional:    true,
+			ForceNew:    true,
 		},
 		"retention_period": {
 			Type:         schema.TypeString,
+			Description:  "Time of data retention in the topic, [ISO 8601](https://ru.wikipedia.org/wiki/ISO_8601) format.",
 			Optional:     true,
 			ValidateFunc: validation.NoZeroValues,
 			ForceNew:     true,
 		},
 		"consumer": {
-			Type:     schema.TypeList,
-			Optional: true,
-			Computed: true,
+			Type:        schema.TypeList,
+			Description: "Changefeed [consumers](https://ydb.tech/en/docs/concepts/topic#consumer) - named entities for reading data from the topic.",
+			Optional:    true,
+			Computed:    true,
 			Elem: &schema.Resource{
 				Schema: map[string]*schema.Schema{
 					"name": {
 						Type:         schema.TypeString,
+						Description:  "Consumer name. It is used in the SDK or CLI to [read data](https://ydb.tech/en/docs/best_practices/cdc#read) from the topic.",
 						Required:     true,
 						ValidateFunc: validation.NoZeroValues,
 					},
 					"supported_codecs": {
-						Type:     schema.TypeList,
-						Optional: true,
+						Type:        schema.TypeList,
+						Description: "Supported data encodings.",
+						Optional:    true,
 						Elem: &schema.Schema{
 							Type:         schema.TypeString,
 							ValidateFunc: validation.StringInSlice(topic.YDBTopicAllowedCodecs, false),
@@ -184,9 +195,10 @@ func ResourceSchema() map[string]*schema.Schema {
 						Computed: true,
 					},
 					"starting_message_timestamp_ms": {
-						Type:     schema.TypeInt,
-						Optional: true,
-						Computed: true,
+						Type:        schema.TypeInt,
+						Description: "Timestamp in the UNIX timestamp format, from which the consumer will start reading data.",
+						Optional:    true,
+						Computed:    true,
 					},
 					"important": {
 						Type:     schema.TypeBool,
